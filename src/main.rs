92a8198329ce0f784b47f702c2f0f269a3ea4c62
gursi26 <gursimar.batra@gmail.com>
2022-03-07 23:11:15 +0800

    optimized infection check and rendering loops

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 extern crate raylib;
 use rand::Rng;
 use raylib::prelude::*;
+use std::time::Instant;
 
 const TARGETFPS: u32 = 50;
 
 // ball params
 const RADIUS: f32 = 5.0;
-const NUMBALLS: i32 = 300;
+const NUMBALLS: i32 = 1500;
 const MAXSPEED: i32 = 3;
 
 // colors 
@@ -16,9 +17,9 @@ const RECOVERED_COLOR: Color = Color::DARKBLUE;
 const BG_COLOR: Color = Color::WHITE;
 
 // infection params
-const INFECTION_RADIUS: f32 = 3.0;
-const INFECTION_RATE: f32 = 0.3;
-const BASE_RECOVERY_TIME: i32 = 6; // time in seconds for recovery/death
+const INFECTION_RADIUS: f32 = 2.5;
+const INFECTION_RATE: f32 = 0.25;
+const BASE_RECOVERY_TIME: i32 = 5; // time in seconds for recovery/death
 const RECOVERY_TIME_RANGE: i32 = 2; // recovery_time = BASE_RECOVERY_TIME + rand(-RECOVERY_TIME_RANGE, RECOVERY_TIME_RANGE)
 
 #[derive(Clone, Copy)]
@@ -63,38 +64,36 @@ impl Ball {
         infected_arr: &mut Vec<Ball>,
         normal_arr: &mut Vec<Ball>,
         recovered_arr: &mut Vec<Ball>,
-    ) {
-        let mut i: i32 = 0;
-        let mut outer_loop_end: i32 = infected_arr.len() as i32; 
-        while i < outer_loop_end {
-            let mut j: i32 = 0;
-            let mut end_idx: i32 = normal_arr.len() as i32;
-            while j < end_idx {
-                if collision::check_collision_circles(
-                    infected_arr[i as usize].pos,
-                    RADIUS + INFECTION_RADIUS,
-                    normal_arr[j as usize].pos,
-                    RADIUS,
-                ) {
-                    let random = rand::thread_rng().gen_range(1..100) as f32 / 100.0;
-                    if random < INFECTION_RATE {
-                        infected_arr.push(normal_arr.remove(j as usize));
-                        j -= 1;
-                        end_idx -= 1;
-                    }
+        mut i: i32, 
+        mut outer_loop_end: i32,
+    ) -> (i32, i32) { 
+        let mut j: i32 = 0;
+        let mut end_idx: i32 = normal_arr.len() as i32;
+        while j < end_idx {
+            if collision::check_collision_circles(
+                infected_arr[i as usize].pos,
+                RADIUS + INFECTION_RADIUS,
+                normal_arr[j as usize].pos,
+                RADIUS,
+            ) {
+                let random = rand::thread_rng().gen_range(1..100) as f32 / 100.0;
+                if random < INFECTION_RATE {
+                    infected_arr.push(normal_arr.remove(j as usize));
+                    j -= 1;
+                    end_idx -= 1;
                 }
-                j += 1;
             }
-            let recovery_time: i32 = BASE_RECOVERY_TIME + rand::thread_rng().gen_range(-RECOVERY_TIME_RANGE..RECOVERY_TIME_RANGE);
-            if infected_arr[i as usize].time_infected >= recovery_time * TARGETFPS as i32 {
-                recovered_arr.push(infected_arr.remove(i as usize));
-                i -= 1; 
-                outer_loop_end -= 1;
-            } else {
-                infected_arr[i as usize].time_infected += 1;
-            }
-            i += 1; 
+            j += 1;
         }
+        let recovery_time: i32 = BASE_RECOVERY_TIME + rand::thread_rng().gen_range(-RECOVERY_TIME_RANGE..RECOVERY_TIME_RANGE);
+        if infected_arr[i as usize].time_infected >= recovery_time * TARGETFPS as i32 {
+            recovered_arr.push(infected_arr.remove(i as usize));
+            i -= 1;
+            outer_loop_end -= 1;
+        } else {
+            infected_arr[i as usize].time_infected += 1;
+        }
+        (i, outer_loop_end)
     }
 
     fn wall_collision(&mut self, width: i32, height: i32) {
@@ -133,6 +132,7 @@ fn draw_stats(d: &mut RaylibDrawHandle, infected_len: usize, normal_len: usize, 
 fn main() {
     let (mut rl, thread) = init().size(1440, 900).fullscreen().title("Pandemic Simulation").build();
     //let (mut rl, thread) = init().size(1420, 827).resizable().title("Pandemic Simulation").build();
+
     let (mut width, mut height) = (rl.get_screen_width(), rl.get_screen_height());
     rl.set_target_fps(TARGETFPS);
 
@@ -142,22 +142,27 @@ fn main() {
     Ball::populate(height, width, &mut infected_arr, &mut normal_arr);
 
     while !rl.window_should_close() {
+        let start = Instant::now(); 
+
         (width, height) = (rl.get_screen_width(), rl.get_screen_height());
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(BG_COLOR);
 
-        // drawing infected balls
-        for i in 0..infected_arr.len() {
-            let ball = &mut infected_arr[i as usize];
-            d.draw_circle_v(ball.pos, RADIUS + 1.0, INFECTED_COLOR);
-            d.draw_circle_v(ball.pos, RADIUS - 1.0, Color::WHITE);
+        let mut i: i32 = 0; 
+        let mut end_idx: i32 = infected_arr.len() as i32;
+        while i < end_idx {
+            d.draw_circle_v(infected_arr[i as usize].pos, RADIUS + 1.0, INFECTED_COLOR);
+            d.draw_circle_v(infected_arr[i as usize].pos, RADIUS - 1.0, Color::WHITE);
 
             // updating positions
-            ball.pos.x += ball.speed.x;
-            ball.pos.y += ball.speed.y;
+            infected_arr[i as usize].pos.x += infected_arr[i as usize].speed.x;
+            infected_arr[i as usize].pos.y += infected_arr[i as usize].speed.y;
 
             // wall collision check and update
-            ball.wall_collision(width, height);
+            infected_arr[i as usize].wall_collision(width, height);
+
+            (i, end_idx) = Ball::infection_check(&mut infected_arr, &mut normal_arr, &mut recovered_arr, i as i32, end_idx as i32);
+            i += 1;
         }
 
         // drawing normal balls
@@ -186,7 +191,9 @@ fn main() {
             ball.wall_collision(width, height);
         }
 
-        Ball::infection_check(&mut infected_arr, &mut normal_arr, &mut recovered_arr);
         draw_stats(&mut d, infected_arr.len(), normal_arr.len(), recovered_arr.len());
+
+        let duration = start.elapsed();
+        println!("Render time for frame: {:?}", duration);
     }
 }
